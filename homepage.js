68dc89f27eb2f9eaa6e4377f23e541556ff8d8d0
Tam8922 <127450535+Tam8922@users.noreply.github.com>
2023-12-28 18:34:34 +0000

    new changes

---
+++ homepage.js
@@ -0,0 +1,20 @@
+$('#arrow-1').on('click', () => {
+    $('#toggle-p-1').slideToggle();
+    $('#arrow-1').toggleClass('rotate');
+}); 
+
+$('#toggle-p-1').hide();
+
+$('#arrow-2').on('click', () => {
+    $('#toggle-p-2').slideToggle();
+    $('#arrow-2').toggleClass('rotate');
+});
+
+$('#toggle-p-2').hide();
+
+$('#arrow-3').on('click', () => {
+    $('#toggle-p-3').slideToggle();
+    $('#arrow-3').toggleClass('rotate');
+});
+
+$('#toggle-p-3').hide();
