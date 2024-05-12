75bb3433524cd2c7d2fcd100c724adcf449423da
Tam8922 <tanpreetdhani@live.co.uk>
2024-05-12 22:45:59 +0100

    additions and iterations
    added segmented buttons and made some corrections.

--- portfolio.js
+++ portfolio.js
@@ -20,8 +20,10 @@ $('#arrow-3').on('click', () => {
 $('#toggle-p-3').hide(); 
 
 
+// -------------------------------------------------------------------------------------------
 
-// Get the button:
+
+// Scroll-to-top button
 let mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
@@ -40,3 +42,42 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+
+// -------------------------------------------------------------------------------------------
+
+
+// HOMEPAGE - segmented button action
+function showContent(sectionNumber) {
+  // Hide all content sections
+  const allContent = document.querySelectorAll('.content');
+  allContent.forEach((section) => {
+      section.style.display = 'none';
+  });
+
+  // Show the selected content section
+  const selectedContent = document.getElementById(`content${sectionNumber}`);
+  selectedContent.style.display = 'block';
+}
+
+// Show default content (e.g., on page load)
+showContent(1);
+
+
+// -------------------------------------------------------------------------------------------
+
+
+// HOMEPAGE - segmented button default and focus changes
+const button1 = document.querySelector('.seg-button-1');
+const button2 = document.querySelector('.seg-button-2');
+
+    button2.addEventListener('focus', () => {
+        button1.style.backgroundColor = 'rgba(136, 135, 135, 0.2)';
+        button1.style.color = 'black'; // Changed text color for .seg-button-1
+    });
+
+    button2.addEventListener('blur', () => {
+        button1.style.backgroundColor = 'rgba(36, 1, 74, 1)';
+        button1.style.color = 'white'; // Reverted text color for .seg-button-1
+    });
+
